6d8fb1447936f03d03eed83bd3e41ab6d730204a
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-03-18 11:21:32 +0100

    T004-Nav

--- src/Components/Grid/Grid.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "wouter";
-
-const Grid = () => {
-  return <></>;
-};
-
-export default Grid;
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Carousel from "../Carousel/Carousel";
+import GridSection from "../Grid/GridSection";
 
 const Homepage = () => {
-  return <></>;
+  return (
+    <>
+      <Carousel
+        images={[
+          "https://i.blogs.es/82d7ef/pokemon/1366_521.jpeg",
+          "https://media.vandal.net/i/1280x720/10-2023/17/202310171423122_3.jpg",
+        ]}
+      />
+      <GridSection />
+    </>
+  );
 };
 
 export default Homepage;
